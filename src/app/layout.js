853451bbbec24823b0f9821a813faba1f8a5fb58
gabriella-guimaraes <gabigara13@gmail.com>
2024-07-04 12:47:37 -0300

    feat: Material UI theme - dark theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import ThemeRender from "./themeRender";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeRender>
+          {children}
+        </ThemeRender>
+      </body>
     </html>
   );
 }
---
+++ src/app/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#A0ADC9",
+      },
+      secondary: {
+        main: "#6F405E",
+      },
+      text: {
+        primary: "#E5E9F0",
+        secondary: "#212121",
+      },
+      background: {
+        default: "#0C0F14",
+      }
+    }
+});
+
+export default theme;
