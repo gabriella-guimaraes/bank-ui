6d6c0d4e55c201185a6a9101096e1d096fc25bbb
gabriella-guimaraes <gabigara13@gmail.com>
2024-07-09 15:46:46 -0300

    feat:  About component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "@/Components/hero/Hero";
+import About from "@/Components/About/About";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
+      <About />
     </main>
   );
 }
