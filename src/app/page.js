c55047099126448b4c4b9616ee5d67b6a83b3fe7
gabriella-guimaraes <gabigara13@gmail.com>
2024-07-12 19:21:30 -0300

    feat: Benefits component

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Hero from "@/Components/hero/Hero";
 import About from "@/Components/About/About";
 import styles from "./page.module.css";
+import Benefits from "@/Components/Benefits/Benefits";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
       <About />
+      <Benefits />
     </main>
   );
 }
